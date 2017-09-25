a65c800245030fae5f6f2c447ac7e40620333207
gweonyongil <kyi4210@daum.net>
2017-09-25 12:41:09 +0900

    update base week5 project

---
+++ Week5Project/app/src/main/java/com/example/caucse/week5project/MenuAdapter.java
@@ -0,0 +1,31 @@
+package com.example.caucse.week5project;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by caucse on 2017-09-25.
+ */
+
+public class MenuAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
---
+++ Week5Project/app/src/main/java/com/example/caucse/week5project/MenuItem.java
@@ -0,0 +1,8 @@
+package com.example.caucse.week5project;
+
+/**
+ * Created by caucse on 2017-09-25.
+ */
+
+public class MenuItem {
+}
